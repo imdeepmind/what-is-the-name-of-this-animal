75505e0da1233ca02dc8b003593c541f2e15c993
imdeepmind <infinityatme@gmail.com>
2019-03-27 22:51:59 +0530

    added script to download all images

---
+++ download.py
@@ -0,0 +1,19 @@
+import urllib
+import os
+
+animals = ['cat', 'dog', 'lion', 'tiger', 'elephant']
+
+for animal in animals:
+    if not os.path.exists('data/images/{}'.format(animal)):
+        os.makedirs('data/images/{}'.format(animal))
+        
+    with open('data/{}.txt'.format(animal)) as txt:
+        images = txt.read()
+        images = images.split('\n')
+        print('--There are total {} {} images'.format(len(images), animal))
+        for i in range(len(images)):
+            try:
+                print('--Downloading {} image number {}--'.format(animal, i))
+                urllib.request.urlretrieve(images[i], 'data/images/{}/{}.jpg'.format(animal,i))
+            except Exception as ex:
+                print('--Something went wrong with the {} image number {} : {}--'.format(animal,i,str(ex)))
